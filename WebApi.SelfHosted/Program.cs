7682559b683a2d9cdfb57c9940547ade3feb22c7
David Ruttka <david.ruttka@gmail.com>
2012-04-05 10:13:21 -0500

    Configure and start server, run until 'q'

--- WebApi.SelfHosted/Program.cs
+++ WebApi.SelfHosted/Program.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Web.Http;
+using System.Web.Http.SelfHost;
+using System;
 
 namespace WebApi.SelfHosted
 {
@@ -9,6 +8,33 @@ namespace WebApi.SelfHosted
     {
         static void Main(string[] args)
         {
+            var config = CreateSelfHostConfiguration();
+            StartServer(config);
+            
+            Console.WriteLine("q to quit");
+            while (string.Compare(Console.ReadLine(), "q", true) != 0);
+        }
+
+        private static HttpSelfHostConfiguration CreateSelfHostConfiguration()
+        {
+            // Setup server configuration 
+            const string baseAddress = "http://localhost:8081/";
+            var config = new HttpSelfHostConfiguration(baseAddress);
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            return config;
+        }
+
+        private static void StartServer(HttpSelfHostConfiguration config)
+        {
+            // Create and open the server 
+            var server = new HttpSelfHostServer(config);
+            server.OpenAsync().Wait();
+            Console.WriteLine("The server is running...");
         }
     }
 }
